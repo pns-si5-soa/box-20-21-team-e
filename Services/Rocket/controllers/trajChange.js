b4271e6ab344fab8df6c1f4e46adff5fffcee4c9
MaelDelaby <maeldelabouy@yahoo.fr>
2020-10-10 07:32:32 +0200

    #39 la trajectoire et la vitesse mettent du temps a se changer

--- Services/Rocket/controllers/trajChange.js
+++ Services/Rocket/controllers/trajChange.js
@@ -1,11 +1,60 @@
 const rocketData = require('../data').rocketData;
 
+var inProcess = false;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms*1000));
+}
+
+async function changeTrajAsync(futurAngle, futurSpeed) {
+    inProcess = true;
+
+    while(rocketData.velocity != futurSpeed || rocketData.angle != futurAngle){
+        await sleep(1);
+
+        if (rocketData.velocity > futurSpeed){
+            if (rocketData.velocity - 5 > futurSpeed){
+                rocketData.velocity = rocketData.velocity - 5;
+            } else {
+                rocketData.velocity = futurSpeed;
+            }
+        } else if (rocketData.velocity < futurSpeed){
+            if (rocketData.velocity + 5 < futurSpeed){
+                rocketData.velocity = rocketData.velocity + 5;
+            } else {
+                rocketData.velocity = futurSpeed;
+            }
+        }
+
+        if (rocketData.angle > futurAngle){
+            if (rocketData.angle - 5 > futurAngle){
+                rocketData.angle = rocketData.angle - 5;
+            } else {
+                rocketData.angle = futurAngle;
+            }
+        } else if (rocketData.angle < futurAngle){
+            if (rocketData.angle + 5 < futurAngle){
+                rocketData.angle = rocketData.angle + 5;
+            } else {
+                rocketData.angle = futurAngle;
+            }
+        }
+    }
+
+    inProcess = false;
+}
+
 const change = async (speed, angle) => {
     try {
-        console.log("rocket : changement de trajectoire : ", angle, " et/ou de vitesse : ", speed);
-        rocketData.vitess = speed;
-        rocketData.angle = angle;
-        return "TRAJCHANGE";
+
+        if (inProcess == true){
+            console.log("rocket : Vous ne pouvez pas changer de trajectoire car elle la rocket n a pas encore fini son dernier changement de trajectoire");
+            return "WAIT THE END OF THE TRAJCHANGE";
+        } else {
+            console.log("rocket : changement de trajectoire : ", angle, " et/ou de vitesse : ", speed);
+            changeTrajAsync(angle, speed);
+            return "TRAJCHANGE";
+        }
     } catch (err) {
         console.error(err);
     }
