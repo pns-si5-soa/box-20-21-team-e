37128688195ad2fe6c2869e12c7707aa014fb135
MaelDelaby <maeldelabouy@yahoo.fr>
2020-10-11 11:29:26 +0200

    #42 ralentissement au bout de 10 seconde de 40 à 30

--- Services/Rocket/controllers/launch.js
+++ Services/Rocket/controllers/launch.js
@@ -1,4 +1,5 @@
 const rocketData = require('../data').rocketData;
+const trajChangeController = require('../controllers/trajChange');
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms*1000));
@@ -16,13 +17,22 @@ async function tankEmptying() {
     }
 }
 
+async function timeCounter() {//Permet d avoir une idée chronologique de l avancer de la fusee
+    while(1){
+        await sleep(1);
+        rocketData.time += 1;
+    }
+}
+
 const launch = async () => {
     try {
         if (rocketData.launch == 0){
             console.log("rocket : rocket launched !")
             rocketData.launch = 1;
             console.log("rocket : le reservoir commence a se vider")
+            await trajChangeController.change(40, 0)
             tankEmptying();
+            timeCounter();
             return "LAUNCHED";
         } else {
             console.log("rocket : ordre de lancer la rocket alors qu elle est deja lancee")
