b5d5009af65ed3b3dc6c38ec636bf0e49f0cc58c
Othmane Mazouz <mazouz.othmane@gmail.com>
2020-10-12 14:47:33 +0200

    log(destroyed)

--- Services/Rocket/controllers/destruction.js
+++ Services/Rocket/controllers/destruction.js
@@ -3,12 +3,12 @@ const rocketData = require('../data').rocketData;
 const destruction = async () => {
     try {
         if (rocketData.destruction == 0){
-            console.log("rocket : Rocket Split !")
+            console.log("rocket : Rocket Destroyed !")
             rocketData.destruction = 1;
             return "DESTRUCTION";
         } else {
             console.log("rocket : ordre de destruction la rocket alors qu elle est deja destruction")
-            return "ALREADY DESTRUCTED";
+            return "ALREADY DESTROYED";
         }
     } catch (err) {
         console.error(err);
