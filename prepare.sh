a699cf601bccba7d383b278126d0463d71622d29
MaelVB <vaillant.beuchot.mael@gmail.com>
2020-11-11 11:04:24 +0100

    Revert

--- prepare.sh
+++ prepare.sh
@@ -1,23 +1,23 @@
-services_list=$(ls ../Services/)
+services_list=$(ls Services/)
 mapfile -t services_array <<< "$services_list"
 
-cd ../Services
+cd Services
 
 for i in "${services_array[@]}"
 do
     cd $i
     pwd
     rm .env
-    cp ../../script/.env.docker ./.env
+    cp ../../.env.docker ./.env
     cd ../
 done
 
-cd ../script
+cd ../
 
 docker-compose up -d
 containerName="client"
 testIsUp=$( docker container inspect -f '{{.State.Running}}' $containerName )
-while [ $testIsUp != "true" ]
+while [[ $testIsUp != "true" ]]
 do
     sleep 2
     testIsUp=$( docker container inspect -f '{{.State.Running}}' $containerName )
